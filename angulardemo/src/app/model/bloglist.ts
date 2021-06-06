1f6da279103d1d73aff632811f3f34beeed51443
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-06-06 21:09:12 +0530

    update: master

--- angulardemo/src/app/model/bloglist.ts
+++ angulardemo/src/app/model/bloglist.ts
@@ -3,7 +3,25 @@ import { Blog } from './bloghub';
 export let blogs: Blog[] = [
     new Blog(
         1,
-        'Spring',
+        'A',
+        'Latest modern solutions for web development',
+        'technical'
+    ),
+    new Blog(
+        2,
+        'B',
+        'Latest modern solutions for web development',
+        'technical'
+    ),
+    new Blog(
+        3,
+        'C',
+        'Latest modern solutions for web development',
+        'technical'
+    ),
+    new Blog(
+        4,
+        'D',
         'Latest modern solutions for web development',
         'technical'
     ),
