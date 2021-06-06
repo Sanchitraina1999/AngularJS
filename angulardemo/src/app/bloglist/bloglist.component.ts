90c038dedd3a718075505ddca294c5bde4b6e885
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-06-06 21:09:47 +0530

    update: master

--- angulardemo/src/app/bloglist/bloglist.component.ts
+++ angulardemo/src/app/bloglist/bloglist.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BloglistComponent implements OnInit {
 
+  blogs: Blog[]
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
