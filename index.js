36dd19969e4a96ec112efaccc3ba12fc881c7146
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-06-06 10:46:28 +0530

    update: master

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var myapp = angular.module('myapp',[])
 
-myapp.controller('mainController',($scope)=>{
+myapp.controller('mainController',($scope,$filter)=>{
     $scope.name = 'Jane Doe'
+    $scope.lower
     console.log($scope)
 })
