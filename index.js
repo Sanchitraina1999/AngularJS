56726168874ee8412b5365606dda29568dcc8c31
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-06-06 19:32:11 +0530

    update: master

--- index.js
+++
@@ -1,7 +0,0 @@
-var myapp = angular.module('myapp',[])
-
-myapp.controller('mainController',($scope,$filter)=>{
-    $scope.name = 'Jane Doe'
-    $scope.nameInLowerCase = $filter('lowercase')($scope.name)
-    console.log($scope)
-})
