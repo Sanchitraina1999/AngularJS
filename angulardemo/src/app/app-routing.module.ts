40b662c6dd0619cc81c319f962119d7349a687d3
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-06-07 08:51:33 +0530

    update: master

--- angulardemo/src/app/app-routing.module.ts
+++ angulardemo/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { BloglistComponent } from './bloglist/bloglist.component';
 import { BlogformComponent } from './blogform/blogform.component';
+import { FormsModule } from '@angular/forms';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), BrowserModule],
+  imports: [RouterModule.forRoot(routes), BrowserModule, FormsModule],
   exports: [RouterModule, BloglistComponent, BlogformComponent],
   declarations: [
     BloglistComponent,
