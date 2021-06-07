7db699f4d78ae54d8f46f44cc4f9ba8ef06200ea
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-06-07 10:18:27 +0530

    update: master

--- angulardemo/src/app/service/blog.service.ts
+++ angulardemo/src/app/service/blog.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Blog } from '../model/bloghub';
+import { blogs } from '../model/bloglist';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
 
-  constructor() { }
+  blogs: Blog[];
+  constructor() { 
+    this.blogs = blogs;
+  }
 }
