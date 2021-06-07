87fff302bab532d7d93233751715931b48e102f1
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-06-07 08:40:50 +0530

    update: master

--- angulardemo/src/app/blogform/blogform.component.ts
+++ angulardemo/src/app/blogform/blogform.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogformComponent implements OnInit {
 
-  constructor() { }
+  categories: string[]
+  constructor() { 
+    this.categories=[];
+  }
 
   ngOnInit(): void {
   }
